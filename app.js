65625a528f5818d62e8afad29778235474c45f94
RAVI SHANKAR PRATIHAST <ravipratihast@gmail.com>
2022-01-27 23:13:17 +0530

    button

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector("#btn-translate");
+// console.log(btnTranslate);
+
+// click event listener
+
+btnTranslate.addEventListener("click",function clickEventHandler(){console.log("clicked")})
