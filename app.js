45cfa738aa026594c9a7f2fe3ec4f3bc8896168a
RAVI SHANKAR PRATIHAST <ravipratihast@gmail.com>
2022-01-28 10:05:53 +0530

    taking input

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+console.log("txt-input");
 // console.log(btnTranslate);
 
 // click event listener
@@ -6,8 +8,10 @@ var btnTranslate = document.querySelector("#btn-translate");
 btnTranslate.addEventListener("click",clickHandler())
 
 function clickHandler()
-
-  
+{
+    return function clickEventHandler()
     {
         console.log("clicked!");
+        console.log("input",txtInput.value);
     }
+}
